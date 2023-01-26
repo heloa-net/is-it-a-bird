3675613f87bdac064bd2414f0cc15be85e3d70aa
Heloá <13964550+heloa-net@users.noreply.github.com>
2023-01-26 16:39:48 -0500

    Add gallery

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import { useState } from 'react';
 import './App.css';
+import data from './data.json';
 
 function App() {
+  const [photos, setPhotos] = useState(data);
+  const [selectedImage, setSelectedImage] = useState(null);
+
   return (
     <div className="App">
+      <h1>Bird Gallery</h1>
+      {selectedImage && (
+        <div>
+          <img src={selectedImage.url}/>
+          <p>
+              <span>by </span>
+              <a href={`https://unsplash.com/@${selectedImage.photographer}`}>{selectedImage.photographer}</a>
+              <br />
+              {selectedImage.location}
+            </p>
+        </div>
+      )}
+      <div className="grid-container">
+        {photos.map((photo, index) => (
+          <div key={index} className="grid-item">
+            <img src={photo.url} onClick={() => setSelectedImage(photo)} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
