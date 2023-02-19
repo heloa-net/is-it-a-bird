0f1e504abea2e4a82b8a8d8ff82d0b48b8d19875
Heloá <13964550+heloa-net@users.noreply.github.com>
2023-02-19 14:02:17 -0500

    Add loading to SelectedImage, restore visual elements, remove sample

--- src/SelectedImage.js
+++ src/SelectedImage.js
@@ -1,12 +1,6 @@
 import React from "react";
 
-const SAMPLE = {
-  "url": "https://images.unsplash.com/photo-1513173125192-da282c9a64ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max",
-  "photographer": "paul_nic",
-  "location": "Hyde Park, London, United Kingdom"
-}
-
-const SelectedImage = ({ image = SAMPLE, inference }) => {
+const SelectedImage = ({ image, inference }) => {
   // Result
   // Is it a bird?
   // What is it?
@@ -23,20 +17,26 @@ const SelectedImage = ({ image = SAMPLE, inference }) => {
 
   return (
     <div>
-      <h1>SelectedImage</h1>
-      {/* <h2>{JSON.stringify(inference)}</h2> */}
+      {
+        inference
+          ? <>
+            <h1>SelectedImage</h1>
+            <h2>{JSON.stringify(inference)}</h2>
+            {image && (
+              <div>
+                <img src={image.url} />
+                <p>
+                  <span>by </span>
+                  <a href={`https://unsplash.com/@${image.photographer}`}>{image.photographer}</a>
+                  <br />
+                  {image.location}
+                </p>
+              </div>
+            )}
+          </>
+          : <h1>Loading...</h1>
+      }
 
-      {/* {image && (
-        <div>
-          <img src={image.url} />
-          <p>
-            <span>by </span>
-            <a href={`https://unsplash.com/@${image.photographer}`}>{image.photographer}</a>
-            <br />
-            {image.location}
-          </p>
-        </div>
-      )} */}
     </div>
   )
 }
